dc10af345382746852e0069e15270b62903edbbb
Bowie <larissa.soaress@yahoo.com.br>
2025-05-21 17:23:07 -0300

    "Implementação de Rotas- finalizada"

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,16 +6,22 @@ function Footer() {
 
     return (
         <>
-            <div className="flex justify-center bg-indigo-900 text-white">
+            <div className="flex justify-center bg-purple-900 text-purple-200">
                 <div className="container flex flex-col items-center py-4">
                     <p className='text-xl font-bold'>
                         Blog Pessoal | Copyright: {data}
                     </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
+                        <a href="https://www.linkedin.com/in/larissa-soares-da-silva/" target='_blank'>
                         <LinkedinLogoIcon size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.instagram.com/larissasoaress27/?next=%2F" target='_blank'>
                         <InstagramLogoIcon size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.facebook.com/larissa.soares.9638/" target='_blank'>
                         <FacebookLogoIcon size={48} weight='bold' />
+                        </a>
                     </div>
                 </div>
             </div>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,8 +2,8 @@
 function Home() {
     return (
         <>
-            <div className="bg-indigo-900 flex justify-center">
-                <div className='container grid grid-cols-2 text-white'>
+            <div className="bg-purple-900 flex justify-center">
+                <div className='container grid grid-cols-2 text-purple-200'>
                     <div className="flex flex-col gap-4 items-center justify-center py-4">
                         <h2 className='text-5xl font-bold'>
                             Seja Bem Vinde!
@@ -13,9 +13,9 @@ function Home() {
                         </p>
 
                         <div className="flex justify-around gap-4">
-                            <div className='rounded text-white 
+                            <div className='rounded text-purple-200
                                             border-white border-solid border-2 py-2 px-4'
-                            >
+                                >
                                 Nova Postagem
                             </div>
                         </div>
