dea5800abedcf6e02160672a26d72a06151ed90d
Bowie <larissa.soaress@yahoo.com.br>
2025-05-29 12:22:24 -0300

    "Implementação CRUD postagem finalizada "

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,5 @@
+import ListaPostagens from "../../components/postagens/listapostagens/ListaPostagens"
+import ModalPostagem from "../../components/postagens/modalpostagem/ModalPostagem"
 
 function Home() {
     return (
@@ -12,11 +14,9 @@ function Home() {
                             Expresse aqui seus pensamentos e opniões
                         </p>
 
-                        <div className="flex justify-around gap-4">
-                            <div className='rounded text-purple-200
-                                            border-white border-solid border-2 py-2 px-4'
-                                >
-                                Nova Postagem
+                        <div className="flex justify-around gap-4 hover:bg-purple-400">
+                            <div className="flex justify-around gap-4  ">
+                               <ModalPostagem />
                             </div>
                         </div>
                     </div>
@@ -30,6 +30,7 @@ function Home() {
                     </div>
                 </div>
             </div>
+            <ListaPostagens />
         </>
     )
 }
--- src/services/Services.ts
+++ src/services/Services.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://blogpessoal-cjr8.onrender.com'
+    baseURL: 'https://blogpessoal-rwjk.onrender.com'
 })
 
 export const cadastrarUsuario = async (url: string, dados: Object, setDados: Function) => {
